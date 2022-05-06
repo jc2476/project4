2f1d457d29fe8882e941b76ef7eb2af661265dcb
jc2476 <97968604+jc2476@users.noreply.github.com>
2022-05-06 16:19:29 -0400

    csv test

--- tests/upload_test.py
+++ tests/upload_test.py
@@ -1,5 +1,13 @@
+import csv
+import os
+
 import pytest
 import unittest
+
+from pylint.testutils.functional import test_file
+
+from app import db, User
+from app.db.models import Transaction
 
 
 class MyTest(unittest.TestCase):
@@ -12,3 +20,27 @@ class MyTest(unittest.TestCase):
         with open("music.csv") as f:
             s = f.read()
         assert "testdata" in s
+
+
+
+def process_csv_test(application):
+    with application.app_context:
+        db.create_all()
+        user = User('joe@joe.com', 'joe123')
+        db.session.add(user)
+        list_of_transactions = []
+        with open(test_file) as file:
+            csv_file = csv.DictReader(file)
+            for row in csv_file:
+                list_of_transactions.append(Transaction(row['Amount'], row['Type']))
+        user.transactions = list_of_transactions
+        db.session.commit()
+        test_transaction = Transaction.query.filter_by(amount='2000').first
+        assert test_transaction.amount == '2000'
+        db.session.delete(user)
+        assert db.session.query(user).count() == 0
+        os.remove(test_file)
+        assert os.path.exists(test_file) == False
+
+
+
